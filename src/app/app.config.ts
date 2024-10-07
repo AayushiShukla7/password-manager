8558bb7c505372bc60977d1cb99621888fd7303a
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-10-07 17:28:26 -0400

    Plugins, installations and configurations completed.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"password-manager-ce0a5","appId":"1:923054135502:web:550ee0193dca3fce13189f","storageBucket":"password-manager-ce0a5.appspot.com","apiKey":"AIzaSyDqmWecEmDJHpbz9jo92hwC6_9mIBjXcKQ","authDomain":"password-manager-ce0a5.firebaseapp.com","messagingSenderId":"923054135502"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
